8c4122b0502d41ad381107c06f40b92fa92302c6
sawl-an-dawnen <elijah.salandanan95@gmail.com>
2022-11-20 15:06:38 -0600

    testing push, moved var in driver file

--- task_group_v2.py
+++ task_group_v2.py
@@ -3,18 +3,17 @@ from generate_map import plotly_heatmap
 from windows_functions import generate_windows, generate_windows_filtered
 
 #pointCount = 50000 #plot dpi
-windowSize = 3 #how many batches to include in each window
 #frameRate = 6 #frameRate of the animation
 #plotType = "nearest"
-my_files_list = list_files('earthquake_contiguous_usa_12batch', '.csv') #list of all batches names
-windows = generate_windows(my_files_list, windowSize) #all window dataframes
 
+windowSize = 3 #how many batches to include in each window
 w_depth = .3 
 w_mag = .7
+
+my_files_list = list_files('earthquake_contiguous_usa_12batch', '.csv') #list of all batches names
+windows = generate_windows(my_files_list, windowSize) #all window dataframes
 
 windows_d1, windows_d2 = generate_windows_filtered(windows,w_depth,w_mag)
 
 plotly_heatmap(windows_d1[0])
-
-
 
